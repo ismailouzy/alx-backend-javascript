0bcf0f53f8074d9aa8b51d3401df8a17171316ea
ismailouzy <ismailouzal@gmail.com>
2024-07-30 17:56:52 +0000

    done

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,9 @@
+const getListStudentIds = (listStudents) => {
+  if (!Array.isArray(listStudents)) {
+    return [];
+  }
+  
+  return listStudents.map(student => student.id);
+};
+
+export default getListStudentIds;
