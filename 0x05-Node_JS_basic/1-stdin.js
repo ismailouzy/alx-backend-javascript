a859fab8ec6d965ffac9fe11a8a48a7b90aa7f21
ismailouzy <ismailouzal@gmail.com>
2024-09-17 17:21:49 +0000

    task1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,18 +1,9 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-let name = '';
-
-process.stdin.on('readable', () => {
-  const chunk = process.stdin.read();
-  if (chunk !== null) {
-    name += chunk;
-  }
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is: ${data}`);
 });
 
 process.stdin.on('end', () => {
-  name = name.trim();
-  if (name) {
-    process.stdout.write(`Your name is: ${name}\n`);
-  }
   process.stdout.write('This important software is now closing\n');
 });
