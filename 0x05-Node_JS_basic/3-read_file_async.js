42d404b11480bcf040ac959ffb7a29489a1822f6
ismailouzy <ismailouzal@gmail.com>
2024-09-17 17:38:31 +0000

    task3

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,41 @@
+const fs = require('fs').promises;
+
+async function countStudents(path) {
+  try {
+    // Read the file asynchronously
+    const data = await fs.readFile(path, 'utf8');
+
+    // Split the data into lines and filter out empty lines
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+
+    // Remove the header line
+    const students = lines.slice(1);
+
+    console.log(`Number of students: ${students.length}`);
+
+    // Create an object to store students by field
+    const fieldCounts = {};
+    const fieldStudents = {};
+
+    students.forEach((student) => {
+      const [firstName, , , field] = student.split(',');
+      if (!fieldCounts[field]) {
+        fieldCounts[field] = 0;
+        fieldStudents[field] = [];
+      }
+      fieldCounts[field] += 1;
+      fieldStudents[field].push(firstName);
+    });
+
+    // Log the count and list of students for each field
+    Object.keys(fieldCounts).forEach((field) => {
+      if (Object.prototype.hasOwnProperty.call(fieldCounts, field)) {
+        console.log(`Number of students in ${field}: ${fieldCounts[field]}. List: ${fieldStudents[field].join(', ')}`);
+      }
+    });
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
