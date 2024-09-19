79f82b87b2ace605b6bef16e86839f90349d7d9f
ismailouzy <ismailouzal@gmail.com>
2024-09-19 22:23:15 +0000

    task6

---
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,28 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const getPaymentTokenFromAPI = require('./6-payment_token');
+
+describe('getPaymentTokenFromAPI', () => {
+  it('should return a resolved promise with the correct data when success is true', (done) => {
+    getPaymentTokenFromAPI(true)
+      .then((result) => {
+        expect(result).to.deep.equal({ data: 'Successful response from the API' });
+        done();
+      })
+      .catch((error) => {
+        done(error);
+      });
+  });
+
+  it('should do nothing when success is false', (done) => {
+    getPaymentTokenFromAPI(false)
+      .then((result) => {
+        expect(result).to.be.undefined;
+        done();
+      })
+      .catch((error) => {
+        done(error);
+      });
+  });
+});
